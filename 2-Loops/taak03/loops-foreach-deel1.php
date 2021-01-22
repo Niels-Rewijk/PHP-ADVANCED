14e09d8b21a5cc6640a0db37daea1a20f52609d8
Niels Rewijk <niels.rewijk@student.rocva.nl>
2021-01-22 11:37:52 +0100

    opdracht 4

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,6 @@
+<?php
+$dagen = array("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+foreach($dagen as $valeu){
+    echo "<li> Vandaag is het $valeu <br /> </li>";
+}
+?>
