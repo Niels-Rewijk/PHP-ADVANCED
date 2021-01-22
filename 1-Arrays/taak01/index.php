caf347621d1085499e73d7ddbf4b50b57eca3345
Niels Rewijk <niels.rewijk@student.rocva.nl>
2021-01-22 11:03:35 +0100

    opdracht 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,24 @@
+<?php
+$piet = array(
+    "voornaam" => "piet ",
+    "achternaam" => "bakker",
+    "leeftijd" => "27",
+    "klas" => "9f"
+);
+
+$muhammad = array(
+    "voornaam" => "muhammad ",
+    "achternaam" => "ali",
+    "leeftijd" => "17",
+    "klas" => "9a"
+);
+
+$els = array(
+    "voornaam" => "els ",
+    "achternaam" => "bakfiets",
+    "leeftijd" => "19",
+    "klas" => "9h"
+);
+
+echo "Dit is " .$piet["voornaam"] .$piet["achternaam"], "hij is " .$piet["leeftijd"], " jaar oud en zit in klas " .$piet["klas"];
+?>  
