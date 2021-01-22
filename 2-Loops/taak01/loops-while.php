77858dada4fb0e3f5ab50045ba512abaf76ce85f
Niels Rewijk <niels.rewijk@student.rocva.nl>
2021-01-22 11:19:49 +0100

    opdracht 2

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,13 @@
+<?php
+$leeftijd = 0;
+
+while($leeftijd < 18){
+    echo '<ul>';
+    echo "<li>Ik ben $leeftijd jaar, dus ik mag nog niet stemmen. <br /></li>";
+    echo '</ul';
+    $leeftijd++;
+}
+echo '<lu>';
+echo "<li> ik ben 18 en mag stemmen </li>";
+echo '</lu>';
+?>
