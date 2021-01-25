ca66211024b8b9f1ad3b3629497d3b3336766235
Niels Rewijk <niels.rewijk@student.rocva.nl>
2021-01-25 11:11:30 +0100

    opdracht 7

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>Gegevens</title>
+        
+    </head>
+    <body>
+        <?php
+        echo "Voornaam is " . $_POST["Voornaam"];
+        echo "<br>";
+        echo "Achternaam is " . $_POST["Achternaam"];
+        echo "<br>";
+        echo "Klas is " . $_POST["Klas"];
+        echo "<br>";
+        echo "leeftijd is " . $_POST["Leeftijd"];
+        echo "<br>";
+        echo "Adres is " . $_POST["Adres"];
+        echo "<br>";
+        echo "Plaatsnaam is " . $_POST["Plaatsnaam"];
+        echo "<br>";
+        echo "Favoriete muziekband is " . $_POST["Favoband"];
+        ?>
+    </body>
+</html>
